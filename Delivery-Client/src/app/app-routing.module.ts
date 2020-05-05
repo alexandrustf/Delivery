9d81da5b26c617a6cf1deb8c34b85e224291a0a6
Razvan <radeanu.razvan99@gmail.com>
2020-05-05 11:17:47 +0300

    included nginx conf

--- Delivery-Client/src/app/app-routing.module.ts
+++ Delivery-Client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnotherComponent } from './another.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'another',
+        component: AnotherComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Delivery-Client/src/app/app.module.ts
+++ Delivery-Client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeliveryFormComponent } from './delivery-form/delivery-form.component';
 import { FormsModule } from '@angular/forms';
+import { AnotherComponent } from './another.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AnotherComponent,
     DeliveryFormComponent
   ],
   imports: [
